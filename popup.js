39c5c7b857d01d7fcd7a307d9d252dc49992d58f
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-04-13 18:48:42 +0100

    Apply functionality to all buttons

--- popup.js
+++ popup.js
@@ -3,22 +3,39 @@ const start = document.getElementById("start");
 const pause = document.getElementById("pause");
 const reset = document.getElementById("reset");
 
+let isCountdownInProgress = false;
+let count = 1500;
+let countdown;
+
 start.addEventListener("click", () => {
-    let count = 1500;
-    
-    const countdown = setInterval(function() {
-        const timeBetween = count--;
+    if (!isCountdownInProgress) {    
+        countdown = setInterval(function() {
+            const timeBetween = count--;
 
-        const minutes = Math.floor((timeBetween / 60));
-        const seconds = Math.floor((timeBetween % 60));
+            const minutes = Math.floor((timeBetween / 60));
+            const seconds = Math.floor((timeBetween % 60));
 
-        let secondsString = seconds.toString().padStart(2, "0");
+            let secondsString = seconds.toString().padStart(2, "0");
 
-        timer.textContent = `${minutes}:${secondsString}`;
+            timer.textContent = `${minutes}:${secondsString}`;
 
-        if (timeBetween < 0) {
-            clearInterval(countdown);
-            timer.textContent = "25:00"
-        }
-    },1000);
+            if (timeBetween < 0) {
+                clearInterval(countdown);
+                timer.textContent = "25:00"
+            }
+        },1000);
+
+        isCountdownInProgress = true;
+    }
+});
+
+pause.addEventListener("click", () => {
+    clearInterval(countdown);
+    isCountdownInProgress = false;
+});
+
+reset.addEventListener("click", () => {
+    clearInterval(countdown);
+    timer.textContent = "25:00"
+    isCountdownInProgress = false;
 });
